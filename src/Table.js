34e206563d80e0b31ec1f8d83526b4b90bfd7ca5
Shengjie Yang <36460626+yangshengjie4u@users.noreply.github.com>
2020-10-22 22:35:28 -0500

    Restructure the Table One by the new response data

--- src/Table.js
+++ src/Table.js
@@ -1,70 +1,70 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import data from './data';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-    maxWidth: 1000
-  },
-});
-
-
 export default function DenseTable() {
-  const classes = useStyles();
 
+    let header=data.headers;
+    let rows=[];
+    data.variables.map((v,k)=>{
+        let row={};
+        row['name']=v.name;
+        row['size']=v.size.total+"("+v.size.true+")";
+        if (v.keys.length==1 && v.keys[0].name==""){
+            row['type']='font-weight-bold';
+            row['true']=v.keys[0].data.true;
+            row['false']=v.keys[0].data.false;
+            row['p']=v.pval;
 
-  let rows=[];
-  data.map((v,k)=>{
-    v['id']=k;
-    v['parent']=0;
-    rows.push(v);
-    if(v.category.length>0){
-      v.category.map((vv,kk)=>{
-        vv['id']=k+"-"+kk;
-        vv['parent']=1;
-        rows.push(vv);
-      });
-    }
-  });
+            rows.push(row);
+        }else {
+            row['type']='font-weight-bold';
+            row['true']='';
+            row['false']='';
+            row['p']=v.pval;
 
+            rows.push(row);
 
+            v.keys.map((vv, kk)=>{
+                let subrow={};
+                subrow['name']=vv.name;
+                subrow['size']='';
+                subrow['type']='pl-5';
+                subrow['true']=vv.data.true;
+                subrow['false']=vv.data.false;
+                subrow['p']='';
+
+                rows.push(subrow);
+            });
+        }
+    });
 
   return (
-      <Table className={classes.table} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Feature</TableCell>
-            <TableCell align="right">Sample Size (SMN) No.</TableCell>
-            <TableCell align="right">SMN Formed, %</TableCell>
-            <TableCell align="right">No SMN Formed, %</TableCell>
-            <TableCell align="right">P-value</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+      <table className="table table-striped">
+          <thead>
+          <tr>
+              <th scope="col"></th>
+              <th scope="col">{header.size}</th>
+              <th scope="col">{header.true}</th>
+              <th scope="col">{header.false}</th>
+              <th scope="col">P-value</th>
+          </tr>
+          </thead>
+          <tbody>
           {rows &&
-            rows.map((v)=>{
-              return(
-                <TableRow key={v.id}>
-                  <TableCell component="th" scope="row" style={v.parent==0?{fontWeight: 'bold'}:{paddingLeft:30}}>
-                    {v.name&&v.name!==''?v.name:''}
-                  </TableCell>
-                  <TableCell align="right">{v.sampleSize&&v.sampleSize!==''?v.sampleSize.toLocaleString():''} {v.SMN&&v.SMN!==''?' ('+v.SMN.toLocaleString()+')':''}</TableCell>
-                  <TableCell align="right">{v.pSMN&&v.pSMN!==''?v.pSMN:''}</TableCell>
-                  <TableCell align="right">{v.pNoSMN&&v.pNoSMN!==''?v.pNoSMN:''}</TableCell>
-                  <TableCell align="right">{v.p&&v.p!==''?v.p:''}</TableCell>
-                </TableRow>
-              )
-            })
+            rows.map((row)=>{
+                  return(
+                      <tr>
+                          <td className={row.type}>{row.name}</td>
+                          <td>{row.size}</td>
+                          <td>{row.true}</td>
+                          <td>{row.false}</td>
+                          <td>{row.p}</td>
+                      </tr>
+                  )
+              })
           }
-        </TableBody>
-      </Table>
+          </tbody>
+      </table>
   );
+
 }
--- src/data.js
+++ src/data.js
@@ -1,7 +1,85 @@
-export default [
-{"name":"Clinical Characteristic", "sampleSize":null, "SMN":null, "category":[]},
-{"name":"Median age at initial diagnosis", "sampleSize":5987, "SMN":43, "pSMN":"27.5 months", "pNoSMN":"18.0 months", "p":0.22, "category":[]},
-{"name":"Age at diagnosis", "sampleSize":5987, "SMN":43, "category":[{"name":">= 18 mo", "pSMN":58.1, "pNoSMN":50, "p":0.27},{"name":"< 18 mo", "pSMN":41.9, "pNoSMN":50, "p":null}]},
-{"name":"Sex", "sampleSize":5987, "SMN":43, "category":[{"name":"Male", "pSMN":37.2, "pNoSMN":53.4, "p":0.07},{"name":"Female", "pSMN":62.8, "pNoSMN":46.6, "p":null}]},
-{"name":"Sex", "sampleSize":5210, "SMN":37, "category":[{"name":"White", "pSMN":80.1, "pNoSMN":84.9, "p":0.08},{"name":"Black", "pSMN":8.1, "pNoSMN":11.8, "p":null},{"name":"Asian/Native", "pSMN":10.8, "pNoSMN":8.3, "p":null}]}
-];
+export default {
+    "headers": {
+        "size": "Sample size (SMN)",
+        "true": "SMN Formed",
+        "false": "No SMN Formed"
+    },
+    "variables": [
+        {
+            "name": "Mean age at diagnosis (mo)",
+            "size": {
+                "total": 5987,
+                "true": 43
+            },
+            "pval": 0.27,
+            "keys": [
+                {
+                    "name": "",
+                    "data": {
+                        "true": 27.5,
+                        "false": 18.0
+                    }
+                }
+            ]
+        },
+        {
+            "name": "Age at diagnosis",
+            "size": {
+                "total": 5987,
+                "true": 43
+            },
+            "pval": 0.27,
+            "keys": [
+                {
+                    "name": "< 18 mo",
+                    "data": {
+                        "true": 41.9,
+                        "false": 50.0
+                    }
+                },
+                {
+                    "name": ">= 18 mo",
+                    "data": {
+                        "true": 58.1,
+                        "false": 50.0
+                    }
+                }
+            ]
+        },
+        {
+            "name": "Sex",
+            "size": {
+                "total": 5987,
+                "true": 43
+            },
+            "pval": 0.07,
+            "keys": [
+                {
+                    "name": "Female",
+                    "data": {
+                        "true": 62.8,
+                        "false": 46.6
+                    }
+                },
+                {
+                    "name": "Male",
+                    "data": {
+                        "true": 37.2,
+                        "false": 53.4
+                    }
+                }
+            ]
+        }
+    ]
+}
+
+
+
+
+//     [
+// {"name":"Clinical Characteristic", "sampleSize":null, "SMN":null, "category":[]},
+// {"name":"Median age at initial diagnosis", "sampleSize":5987, "SMN":43, "pSMN":"27.5 months", "pNoSMN":"18.0 months", "p":0.22, "category":[]},
+// {"name":"Age at diagnosis", "sampleSize":5987, "SMN":43, "category":[{"name":">= 18 mo", "pSMN":58.1, "pNoSMN":50, "p":0.27},{"name":"< 18 mo", "pSMN":41.9, "pNoSMN":50, "p":null}]},
+// {"name":"Sex", "sampleSize":5987, "SMN":43, "category":[{"name":"Male", "pSMN":37.2, "pNoSMN":53.4, "p":0.07},{"name":"Female", "pSMN":62.8, "pNoSMN":46.6, "p":null}]},
+// {"name":"Sex", "sampleSize":5210, "SMN":37, "category":[{"name":"White", "pSMN":80.1, "pNoSMN":84.9, "p":0.08},{"name":"Black", "pSMN":8.1, "pNoSMN":11.8, "p":null},{"name":"Asian/Native", "pSMN":10.8, "pNoSMN":8.3, "p":null}]}
+// ];
